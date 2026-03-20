abf8451f298fca0f2ff10933e9e404427242a714
ayu-ch <ayu.chandekar@gmail.com>
2026-03-20 19:17:11 +0530

    Fix the api routes for auth
    Signed-off-by: ayu-ch <ayu.chandekar@gmail.com>

--- lib/api/axios/index.ts
+++ lib/api/axios/index.ts
@@ -1,4 +1,4 @@
-import { API_BASE_URL } from '@/lib/constants';
+import { API_BASE_URL, AUTH_BASE_URL } from '@/lib/constants';
 import axios, { AxiosInstance } from 'axios';
 import { createAuthenticatedServerAxios } from './server-axios';
 import { createAuthenticatedClientAxios } from './client-axios';
@@ -7,6 +7,15 @@ import { isServer } from '@tanstack/react-query';
 export function getUnauthenticatedAxios() {
   const axiosInstance = axios.create({
     baseURL: API_BASE_URL,
+    timeout: 5000,
+  });
+
+  return axiosInstance;
+}
+
+export function getUnauthenticatedAuthAxios() {
+  const axiosInstance = axios.create({
+    baseURL: AUTH_BASE_URL,
     timeout: 5000,
   });
 
